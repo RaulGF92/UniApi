7863cb3c84ea3f99c6d57e28c86a01fbdb48f62b
raul <raulgf92@gmail.com>
2017-01-24 13:36:58 +0100

    Añadido un path para saber todas las peticiones REST

--- uniapi_backend_apirest/src/main/java/es/uniapi/modules/apirest/controller/HomeController.java
+++ uniapi_backend_apirest/src/main/java/es/uniapi/modules/apirest/controller/HomeController.java
@@ -1,19 +1,29 @@
 package es.uniapi.modules.apirest.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.swing.RepaintManager;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import es.uniapi.modules.apirest.model.Message;
 import es.uniapi.modules.apirest.model.SessionGestionException;
@@ -32,9 +42,22 @@ public class HomeController {
 	private final AtomicLong counter = new AtomicLong();
 	SessionGestor sessionGestor;
 	
+	 private final RequestMappingHandlerMapping handlerMapping;
+
+	 @Autowired
+	 public HomeController(RequestMappingHandlerMapping handlerMapping) {
+		 this.handlerMapping = handlerMapping;
+	 }
+	 
 	@RequestMapping(value="/")
-	public String test(HttpServletResponse response) throws IOException{
-		return "{id:"+counter.incrementAndGet()+",message:'¿Hola que tal? parece que esto funciona! : )'}";
+	public ArrayList<String[]> test(HttpServletResponse response) throws IOException{
+		ArrayList<String[]> patterns=new ArrayList<String[]>();
+		Iterator<RequestMappingInfo> it=this.handlerMapping.getHandlerMethods().keySet().iterator();
+		while(it.hasNext()){
+			PatternsRequestCondition pc=it.next().getPatternsCondition();
+			patterns.add(pc.getPatterns().toArray(new String[pc.getPatterns().size()]));
+		}
+		return patterns;
 	}
 	
 	@RequestMapping(value="/{user}/{pass}")
